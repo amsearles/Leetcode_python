d1bd43da82e12f842bd9cb121fda70c5b21200de
amsearles <asearles@mail.sfsu.edu>
2019-03-29 10:51:48 -0700

    added second solution

--- SortArrayByParity.py
+++ SortArrayByParity.py
@@ -17,3 +17,9 @@ class Solution:
                 B.append(A[x])
                 
         return B
+#Common theme among these is everything can be done in a line or two.
+#second solution while trying to do it only in the return statement. No need to create an entirely new array for it.
+#This solution is only one line but its slower and uses slightly more memory
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+       return ([x for x in A if x%2 == 0] + [x for x in A if x%2 == 1])
