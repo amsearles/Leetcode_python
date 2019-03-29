eae54c26a3ea6370a144d035dea2f11f1284102b
amsearles <asearles@mail.sfsu.edu>
2019-03-29 13:20:55 -0700

    Update ArrayParityTwo.py

--- ArrayParityTwo.py
+++ ArrayParityTwo.py
@@ -9,7 +9,7 @@ You may return any answer array that satisfies this condition.
 
 class Solution:
     def sortArrayByParityII(self, A: List[int]) -> List[int]:
-        result = [None] * len(A)
-        result[::2] = (x for x in A if x%2 == 0)
-        result[1::2] = (x for x in A if x%2 == 1)
+        result = [None] * len(A) #creating empty list with same length as A
+        result[::2] = (x for x in A if x%2 == 0) #for every even index, add an even integer
+        result[1::2] = (x for x in A if x%2 == 1) #for every odd index, add an odd integer
         return result
