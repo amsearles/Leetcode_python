1f4ce81dbb793a00c2467662ae7057d38851b268
amsearles <asearles@mail.sfsu.edu>
2019-03-29 09:55:04 -0700

    Update SquareOfSortedArrays.py

--- SquareOfSortedArrays.py
+++ SquareOfSortedArrays.py
@@ -1,5 +1,6 @@
 #Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
 
+# first solution, terrible. I have to keep in mind the most simplest and efficient solution and not overthink
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
         B = []
@@ -8,3 +9,7 @@ class Solution:
         
         return sorted(B)
         
+#second solution, time complexity is O(NlogN)
+class Solution(object):
+    def sortedSquares(self, A):
+        return sorted(x*x for x in A)
